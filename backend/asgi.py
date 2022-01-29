1aaa8e9cb9a24136b42359b89dfa30f6a871dcd5
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-29 20:03:31 +0900

    websocketのurlを修正

--- backend/asgi.py
+++ backend/asgi.py
@@ -27,7 +27,7 @@ application = ProtocolTypeRouter({
         [
             'http://localhost:3000',
             'http://127.0.0.1:3000',
-            'https://recursion-group-m.github.io'
+            'https://oekaki-app.netlify.app'
         ]
     )
 })
